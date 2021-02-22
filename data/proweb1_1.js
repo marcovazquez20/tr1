a86368908f709cd37bdaa729740af295600ae1d3
marcovazquez20 <31548054+marcovazquez20@users.noreply.github.com>
2021-02-22 12:45:16 -0600

    Add files via upload

---
+++ data/proweb1_1.js
@@ -0,0 +1,10 @@
+var json_proweb1_1 = {
+"type": "FeatureCollection",
+"name": "proweb1_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "qc_id": 0.0, "id": "92b1b323-541b-4bee-a909-99844187d72d", "name": "RY47333_foto-02.jpg", "date": "2021-02-18", "time": "09:47:44.000", "lon": -98.35932, "lat": 26.07816, "camera mak": null, "title": null, "comment": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -98.35932, 26.07816 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 1.0, "id": "26b7e41c-6c50-422f-8120-4ef0913280e7", "name": "RY47334_foto-02.jpg", "date": "2021-02-18", "time": "09:48:14.000", "lon": -98.35332, "lat": 26.06067, "camera mak": null, "title": null, "comment": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -98.35332, 26.06067 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 2.0, "id": "ea8c367a-077a-45fd-830d-d04dcfe3bcf8", "name": "RY48331_foto-02.jpg", "date": "2021-02-18", "time": "09:48:46.000", "lon": -98.31145, "lat": 26.06248, "camera mak": null, "title": null, "comment": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -98.31145, 26.06248 ] ] } }
+]
+}
